c1bda1e05f6d6834a93c3a4351aa18c178ba11fa
Devin Pile <devinpile@gmail.com>
2022-08-24 19:33:16 -0600

    Refactor player and turn

--- lib/turn.rb
+++ lib/turn.rb
@@ -18,45 +18,61 @@ class Turn
       :basic
     end
   end
-
+  
   def winner
-    if    type == :mutually_assured_destruction
+    if type == :mutually_assured_destruction
       "There is no winner"
     elsif type == :war
-      if player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
-        @player1
-      else
-        @player2
-      end
+      war_winner
     else  type == :basic
-      if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
-        @player1
-      else
-        @player2
-      end
+      basic_winner     
+    end
+  end
+  
+  def war_winner
+    if player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
+      @player1
+    else
+      @player2
     end
   end
 
-  def pile_cards
-    if    type == :basic
-      @spoils_of_war << player1.deck.cards[0]
-      player1.deck.remove_card
-      @spoils_of_war << player2.deck.cards[0]
-      player2.deck.remove_card
-    elsif type == :war
-      @spoils_of_war << player1.deck.cards[0..2]
-      player1.deck.remove_card
-      player1.deck.remove_card
-      player1.deck.remove_card
-      @spoils_of_war << player2.deck.cards[0..2]
-      player2.deck.remove_card
-      player2.deck.remove_card
-      player2.deck.remove_card
-      @spoils_of_war.flatten!
-    else  type == :mutually_assured_destruction
-      player1.deck.cards.shift(3)
-      player2.deck.cards.shift(3)
+  def basic_winner
+    if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
+      @player1
+    else
+      @player2
     end
+  end
+  
+  def pile_cards
+    if type == :basic
+      basic_spoils
+    elsif type == :war
+      war_spoils
+    else  type == :mutually_assured_destruction
+      mad_spoils
+    end
+  end
+  
+  def basic_spoils
+    @spoils_of_war << player1.deck.cards[0]
+    player1.deck.remove_card
+    @spoils_of_war << player2.deck.cards[0]
+    player2.deck.remove_card
+  end
+  
+  def war_spoils
+    @spoils_of_war << player1.deck.cards[0..2]
+    3.times { player1.deck.remove_card }
+    @spoils_of_war << player2.deck.cards[0..2]
+    3.times { player2.deck.remove_card }
+    @spoils_of_war.flatten!
+  end
+  
+  def mad_spoils
+    player1.deck.cards.shift(3)
+    player2.deck.cards.shift(3)
   end
 
   def award_spoils(winner)
